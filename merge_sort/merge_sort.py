19fbe83f9ca71cc9ce02c878b07f3fea4dab6554
wigisich <battuhanfidan@gmail.com>
2023-11-19 23:30:23 +0300

    added merge function to merge_sort

---
+++ merge_sort/merge_sort.py
@@ -0,0 +1,29 @@
+# It is easy to merge two sorted arrays. Hence, let's first implement the merge function
+def merge(x, y):
+    i, j = 0, 0
+    merged = []
+    while i<len(x) or j<len(y):
+        if x[i]<y[j]:
+            merged.append(x[i])
+            if i+1<len(x):
+                i += 1
+            else:
+                merged.extend(y[j:])
+                break
+        else:
+            merged.append(y[j])
+            if j+1<len(y):
+                j += 1
+            else:
+                merged.extend(x[i:])
+                break
+    return merged
+
+# A simple recursion function to sort any array
+def merge_sort(ls: list[int]) -> list[int]:
+    if len(ls) <= 1:
+        return ls
+
+    left = merge_sort(ls[:len(ls)//2])
+    right = merge_sort(ls[len(ls)//2:])
+    return merge(left, right)
