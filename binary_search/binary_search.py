c0fc085857a3ad69ddd83cdad6271c69e9674c46
wigisich <battuhanfidan@gmail.com>
2023-11-20 23:41:51 +0300

    commented binary_search

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -3,7 +3,7 @@ def binary_search(item, arr, pointer_low, pointer_high):
     # Because, for example, if we try to declare them inside the recursion, it will create a new search space for each recursive call.
     # This would introduce an unnecessary complexity with the loss of the information about the index we currently are.
 
-    if pointer_low <= pointer_high: # Check the order of the pointers
+    if pointer_low <= pointer_high:                                 # Check the order of the pointers
         mid = (pointer_low + pointer_high) // 2
 
         if arr[mid] == item:
@@ -13,4 +13,4 @@ def binary_search(item, arr, pointer_low, pointer_high):
         else:
             return binary_search(item, arr, pointer_low, mid-1)     # Take the first half for the recursion
     else:
-        return -1 # In case given pointers are incorrect
+        return -1                                                   # In case given pointers are incorrect
